4929d090b64ed4bb874483199a81646b7fd65ff3
Andrew Carlson <acarl005@g.ucla.edu>
2018-01-15 02:42:59 -0800

    get most of the info printing. still need to sort

--- colors.go
+++ colors.go
@@ -5,19 +5,21 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+
+	set "gopkg.in/fatih/set.v0"
 )
 
-func fg(code int) string {
+func Fg(code int) string {
 	colored := []string{"\x1b[38;5;", strconv.Itoa(code), "m"}
 	return strings.Join(colored, "")
 }
 
-func bg(code int) string {
+func Bg(code int) string {
 	colored := []string{"\x1b[48;5;", strconv.Itoa(code), "m"}
 	return strings.Join(colored, "")
 }
 
-func rgb2code(r int, g int, b int) int {
+func Rgb2code(r int, g int, b int) int {
 	code := 36*r + 6*g + b + 16
 	if code < 16 || 231 < code {
 		panic(errors.New(fmt.Sprintf("Invalid RGB values (%i, %i, %i)", r, g, b)))
@@ -25,7 +27,7 @@ func rgb2code(r int, g int, b int) int {
 	return code
 }
 
-func gray2code(lightness int) int {
+func Gray2code(lightness int) int {
 	code := lightness + 232
 	if code < 232 || 255 < code {
 		panic(errors.New(fmt.Sprintf("Invalid lightness value (%i) for gray", lightness)))
@@ -33,42 +35,42 @@ func gray2code(lightness int) int {
 	return code
 }
 
-func fgRGB(r int, g int, b int) string {
-	return fg(rgb2code(r, g, b))
+func FgRGB(r int, g int, b int) string {
+	return Fg(Rgb2code(r, g, b))
 }
 
-func bgRGB(r int, g int, b int) string {
-	return bg(rgb2code(r, g, b))
+func BgRGB(r int, g int, b int) string {
+	return Bg(Rgb2code(r, g, b))
 }
 
-func fgGray(lightness int) string {
-	return fg(gray2code(lightness))
+func FgGray(lightness int) string {
+	return Fg(Gray2code(lightness))
 }
 
-func bgGray(lightness int) string {
-	return bg(gray2code(lightness))
+func BgGray(lightness int) string {
+	return Bg(Gray2code(lightness))
 }
 
-var (
+const (
 	Reset = "\x1b[0m"
 	Bold  = "\x1b[1m"
 )
 
 /*
     'coffee':   [ bold+fgc(136),  fgc(130) ]
-    'js':       [ bold+fg(4,4,0), fg(2,2,0) ]
-    'json':     [ bold+fg(4,4,0), fg(2,2,0) ]
+    'js':       [ bold+Fg(4,4,0), Fg(2,2,0) ]
+    'json':     [ bold+Fg(4,4,0), Fg(2,2,0) ]
     'cson':     [ bold+fgc(136),  fgc(130) ]
     'jsx':      [ bold+fgc(14),   fgc(6) ]
-    'plist':    [ bold+fg(4,0,4), fg(2,0,2) ]
-    'sh':       [ bold+fg(4,0,4), fg(2,0,2) ]
-    'bash':     [ bold+fg(4,0,4), fg(2,0,2) ]
-    'zsh':      [ bold+fg(4,0,4), fg(2,0,2) ]
-    'cpp':      [ bold+fg(4,0,4), fg(2,0,2) ]
-    'h':        [ bold+fg(4,0,4), fg(2,0,2) ]
-    'py':       [ bold+fg(0,3,0), fg(0,1,0) ]
+    'plist':    [ bold+Fg(4,0,4), Fg(2,0,2) ]
+    'sh':       [ bold+Fg(4,0,4), Fg(2,0,2) ]
+    'bash':     [ bold+Fg(4,0,4), Fg(2,0,2) ]
+    'zsh':      [ bold+Fg(4,0,4), Fg(2,0,2) ]
+    'cpp':      [ bold+Fg(4,0,4), Fg(2,0,2) ]
+    'h':        [ bold+Fg(4,0,4), Fg(2,0,2) ]
+    'py':       [ bold+Fg(0,3,0), Fg(0,1,0) ]
     'pyc':      [      fw(8),     fw(5) ]
-    'rb':       [ bold+fg(5,1,0), fg(3,0,0) ]
+    'rb':       [ bold+Fg(5,1,0), Fg(3,0,0) ]
     'java':     [ bold+fgc(136),  fgc(130) ]
     'go':       [ bold+fgc(123),  fgc(117) ]
     'scala':    [ bold+fgc(124),  fgc(88) ]
@@ -82,42 +84,100 @@ var (
     'html':     [      fgc(87),   fgc(73) ]
     'css':      [      fgc(219),  fgc(207) ]
     'scss':     [      fgc(219),  fgc(207) ]
-    'tar':      [      fg(5,0,0), fg(3,0,0) ]
-    'gz':       [      fg(5,0,0), fg(3,0,0) ]
-    'tgz':      [      fg(5,0,0), fg(3,0,0) ]
-    'zip':      [      fg(5,0,0), fg(3,0,0) ]
-    'rar':      [      fg(5,0,0), fg(3,0,0) ]
+    'tar':      [      Fg(5,0,0), Fg(3,0,0) ]
+    'gz':       [      Fg(5,0,0), Fg(3,0,0) ]
+    'tgz':      [      Fg(5,0,0), Fg(3,0,0) ]
+    'zip':      [      Fg(5,0,0), Fg(3,0,0) ]
+    'rar':      [      Fg(5,0,0), Fg(3,0,0) ]
     #
     '_default': [      fw(23),    fw(12) ]
-    '_dir':     [ bold+BG(0,0,2)+fw(23), fg(1,1,5), fg(2,2,5) ]
-    '_.dir':    [ bold+BG(0,0,1)+fw(23), bold+BG(0,0,1)+fg(1,1,5), bold+BG(0,0,1)+fg(2,2,5) ]
-    '_link':    { 'arrow': fg(1,0,1), 'path': fg(4,0,4), 'broken': BG(5,0,0)+fg(5,5,0) }
+    '_dir':     [ bold+BG(0,0,2)+fw(23), Fg(1,1,5), Fg(2,2,5) ]
+    '_.dir':    [ bold+BG(0,0,1)+fw(23), bold+BG(0,0,1)+Fg(1,1,5), bold+BG(0,0,1)+Fg(2,2,5) ]
+    '_link':    { 'arrow': Fg(1,0,1), 'path': Fg(4,0,4), 'broken': BG(5,0,0)+Fg(5,5,0) }
     '_arrow':     fw(1)
-    '_header':  [ bold+BW(2)+fg(3,2,0),  fw(4), bold+BW(2)+fg(5,5,0) ]
+    '_header':  [ bold+BW(2)+Fg(3,2,0),  fw(4), bold+BW(2)+Fg(5,5,0) ]
     '_media':   [      fgc(141),  fgc(54) ]
     #
     '_size':    { b: fgc(20), kB: fgc(33), MB: fgc(81), GB: fgc(123) }
-    '_users':   { root:  fg(5,0,2), default: fg(0,3,3) }
-    '_groups':  { wheel: fg(3,0,0), staff: fg(0,2,0), admin: fg(2,2,0), default: fg(2,0,2) }
-    '_error':   [ bold+BG(5,0,0)+fg(5,5,0), bold+BG(5,0,0)+fg(5,5,5) ]
+    '_users':   { root:  Fg(5,0,2), default: Fg(0,3,3) }
+    '_groups':  { wheel: Fg(3,0,0), staff: Fg(0,2,0), admin: Fg(2,2,0), default: Fg(2,0,2) }
+    '_error':   [ bold+BG(5,0,0)+Fg(5,5,0), bold+BG(5,0,0)+Fg(5,5,5) ]
 
 mediaTypes = new Set ['png', 'gif', 'jpg', 'jpeg', 'ico', 'svg', 'webp', 'tiff', 'pxm', 'mp3', 'm4a', 'wav', 'webm', 'avi', 'wmv', 'mkv', 'mp4', 'flv', 'mov']
 */
 var (
+	MediaTypes    = set.New("png", "gif", "jpg", "jpeg", "ico", "svg", "webp", "tiff", "pxm", "mp3", "m4a", "wav", "webm", "avi", "wmv", "mkv", "mp4", "flv", "mov")
+	CompressTypes = set.New("tar", "gz", "tgz", "zip", "rar")
+	FileColor     = map[string][2]string{
+		"coffee":    [2]string{Bold + Fg(136), Fg(130)},
+		"js":        [2]string{Bold + FgRGB(4, 4, 0), FgRGB(2, 2, 0)},
+		"json":      [2]string{Bold + FgRGB(4, 4, 0), FgRGB(2, 2, 0)},
+		"cson":      [2]string{Bold + Fg(136), Fg(130)},
+		"jsx":       [2]string{Bold + Fg(14), Fg(6)},
+		"plist":     [2]string{Bold + FgRGB(4, 0, 4), FgRGB(2, 0, 2)},
+		"sh":        [2]string{Bold + FgRGB(4, 0, 4), FgRGB(2, 0, 2)},
+		"bash":      [2]string{Bold + FgRGB(4, 0, 4), FgRGB(2, 0, 2)},
+		"zsh":       [2]string{Bold + FgRGB(4, 0, 4), FgRGB(2, 0, 2)},
+		"py":        [2]string{Bold + FgRGB(0, 3, 0), FgRGB(0, 1, 0)},
+		"pyc":       [2]string{FgGray(8), FgGray(5)},
+		"rb":        [2]string{Bold + FgRGB(5, 1, 0), FgRGB(3, 0, 0)},
+		"java":      [2]string{Bold + Fg(136), Fg(130)},
+		"go":        [2]string{Bold + Fg(121), Fg(115)},
+		"scala":     [2]string{Bold + Fg(124), Fg(88)},
+		"c":         [2]string{Bold + Fg(99), Fg(97)},
+		"cpp":       [2]string{Bold + Fg(99), Fg(97)},
+		"h":         [2]string{Bold + Fg(99), Fg(97)},
+		"php":       [2]string{Bold + Fg(30), Fg(22)},
+		"log":       [2]string{FgGray(8), FgGray(5)},
+		"swp":       [2]string{FgGray(8), FgGray(5)},
+		"md":        [2]string{Fg(87), Fg(73)},
+		"markdown":  [2]string{Fg(87), Fg(73)},
+		"html":      [2]string{Fg(87), Fg(73)},
+		"xml":       [2]string{Fg(87), Fg(73)},
+		"css":       [2]string{Fg(219), Fg(207)},
+		"scss":      [2]string{Fg(219), Fg(207)},
+		"_compress": [2]string{FgRGB(5, 0, 0), FgRGB(3, 0, 0)},
+		"_media":    [2]string{Fg(141), Fg(54)},
+		"_default":  [2]string{FgGray(23), FgGray(12)},
+	}
+	SizeColor = map[string]string{
+		" B": Fg(27),
+		"kB": Fg(33),
+		"MB": Fg(81),
+		"GB": Fg(123),
+		"TB": Fg(159),
+	}
 	ConfigColor = map[string]map[string]string{
 		"dir": map[string]string{
-			"name": Bold + bgRGB(0, 0, 2) + fgGray(23),
-			"ext":  fgRGB(2, 2, 5),
+			"name": Bold + BgRGB(0, 0, 2) + FgGray(23),
+			"ext":  FgRGB(2, 2, 5),
 		},
 		".dir": map[string]string{
-			"name": Bold + bgRGB(0, 0, 1) + fgGray(23),
-			"ext":  fgRGB(2, 2, 5),
+			"name": Bold + BgRGB(0, 0, 1) + FgGray(23),
+			"ext":  FgRGB(2, 2, 5),
 		},
 		"folderHeader": map[string]string{
-			"arrow":      fg(58),
-			"main":       bgGray(2) + fgRGB(3, 2, 0),
-			"slash":      fgGray(4),
-			"lastFolder": Bold + fgRGB(5, 5, 0),
+			"arrow":      FgRGB(3, 2, 0),
+			"main":       BgGray(2) + FgRGB(3, 2, 0),
+			"slash":      FgGray(5),
+			"lastFolder": Bold + FgRGB(5, 5, 0),
+		},
+	}
+	PermsColor = map[string]map[string]string{
+		"user": map[string]string{
+			"root":     FgRGB(5, 0, 2),
+			"daemon":   FgRGB(4, 2, 1),
+			"_self":    FgRGB(0, 4, 0),
+			"_default": FgRGB(0, 3, 3),
+		},
+		"group": map[string]string{
+			"wheel":    FgRGB(3, 0, 0),
+			"staff":    FgRGB(0, 2, 0),
+			"admin":    FgRGB(2, 2, 0),
+			"_default": FgRGB(2, 0, 2),
+		},
+		"other": map[string]string{
+			"_default": BgGray(2) + FgGray(15),
 		},
 	}
 )
--- ls-go.go
+++ ls-go.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
+	"os/user"
+	"path"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
+	"syscall"
+	"time"
 
+	"github.com/willf/pad"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -32,9 +39,21 @@ type arguments struct {
 	find     *string
 }
 
+type DisplayItem struct {
+	display  string
+	info     os.FileInfo
+	basename string
+	ext      string
+	link     string
+}
+
 // configure the arguments
 var (
-	args = arguments{
+	True       = true
+	sizeUnits  = []string{" B", "kB", "MB", "GB", "TB"}
+	dateFormat = "02.Jan'06"
+	timeFormat = "15:04:05"
+	args       = arguments{
 		kingpin.Arg("paths", "the files(s) and/or folder(s) to display").Default(".").Strings(),
 		kingpin.Flag("all", "show hidden files").Short('a').Bool(),
 		kingpin.Flag("bytes", "include size").Short('b').Bool(),
@@ -55,12 +74,22 @@ var (
 	}
 )
 
+func argsPostParse() {
+	if *args.long {
+		args.bytes = &True
+		args.mdate = &True
+		args.owner = &True
+		args.perms = &True
+	}
+}
+
 func main() {
 	// auto-generate help text for the command with -h
 	kingpin.CommandLine.HelpFlag.Short('h')
 
 	// parse the arguments and populate the struct
 	kingpin.Parse()
+	argsPostParse()
 
 	// separate the directories from the regular files
 	dirs := []string{}
@@ -113,39 +142,220 @@ func listDir(path string) {
 	if len(items) > 0 {
 		listFiles(path, &items)
 	}
+
+	if *args.recurse {
+		for _, item := range items {
+			if item.IsDir() {
+				listDir(path + item.Name() + "/")
+			}
+		}
+	}
 }
 
 func listFiles(parentDir string, items *[]os.FileInfo) {
-	fmt.Println(parentDir)
+	//prettyPath := prettifyPath(parentDir)
+	absPath, err := filepath.Abs(parentDir)
+	check(err)
+	files := []DisplayItem{}
+	dirs := []DisplayItem{}
+
+	longestOwnerName := 0
+	longestGroupName := 0
+	if *args.owner {
+		for _, fileInfo := range *items {
+			owner, group := getOwnerAndGroup(&fileInfo)
+			longestOwnerName = max(longestOwnerName, len(owner))
+			longestGroupName = max(longestGroupName, len(group))
+		}
+	}
+
 	for _, fileInfo := range *items {
-		fmt.Println(fileInfo.Name())
+		displayItem := DisplayItem{info: fileInfo}
+		filePath := path.Join(absPath, fileInfo.Name())
+
+		var basename, ext string
+		// if this is a dotfile (hidden file)
+		if fileInfo.Name()[0] == '.' {
+			// we can skip everything with this file if we aren't using the `all` option
+			if !*args.all {
+				continue
+			}
+			ext = fileInfo.Name()[1:]
+			basename = ""
+		} else {
+			ext = filepath.Ext(fileInfo.Name())
+			basename = fileInfo.Name()[:len(fileInfo.Name())-len(ext)]
+			if ext != "" {
+				ext = ext[1:]
+			}
+		}
+		displayItem.ext = ext
+		displayItem.basename = basename
+
+		if fileInfo.IsDir() {
+			dirs = append(dirs, displayItem)
+		} else {
+			files = append(files, displayItem)
+		}
+
+		owner, group := getOwnerAndGroup(&fileInfo)
+		ownerColor, groupColor := getOwnerAndGroupColors(owner, group)
+
+		if *args.perms {
+			displayItem.display += permString(fileInfo, ownerColor, groupColor)
+		}
+
+		if *args.owner {
+			ownerInfo := []string{Reset, ownerColor + owner, groupColor + group, Reset}
+			displayItem.display += strings.Join(ownerInfo, " ")
+		}
+
+		if *args.bytes {
+			displayItem.display += sizeString(fileInfo.Size())
+		}
+
+		if *args.mdate {
+			displayItem.display += timeString(fileInfo.ModTime())
+		}
+
+		if fileInfo.IsDir() {
+			displayItem.display += dirString(&displayItem)
+		} else {
+			displayItem.display += fileString(&displayItem)
+		}
+
+		if *args.links && fileInfo.Mode()&os.ModeSymlink != 0 {
+			link, err := os.Readlink(filePath)
+			fmt.Println(link)
+			check(err)
+			displayItem.link = link
+		}
+		fmt.Println(displayItem.display)
+		//fmt.Println(fileInfo.Name())
 	}
 }
 
 /*
-   s = colors['_arrow'] + "►" + colors['_header'][0] + " "
-   ps = path.resolve(ps) if ps[0] != '~'
-   if _s.startsWith(ps, process.env.PWD)
-       ps = "./" + ps.substr(process.env.PWD.length)
-   else if _s.startsWith(p, process.env.HOME)
-       ps = "~" + p.substr(process.env.HOME.length)
+linkString = (file) ->
+    reset + colors['_link']['arrow'] + " ► " + colors['_link'][(file in stats.brokenLinks) and 'broken' or 'path'] + fs.readlinkSync(file)
 
-   if ps == '/'
-       s += '/'
-   else
-       sp = ps.split('/')
-       s += colors['_header'][0] + sp.shift()
-       while sp.length
-           pn = sp.shift()
-           if pn
-               s += colors['_header'][1] + '/'
-               s += colors['_header'][sp.length == 0 and 2 or 0] + pn
-   log reset
-   log s + " " + reset
-   log reset
+nameString = (name, ext, stat) ->
+    key = if mediaTypes.has(ext) then "_media" else ext
+    name = " " + colors[colors[key]? and key or '_default'][0] + name + reset
+    if ((stat.mode >> 6) & 0b001) && args.icons
+        ' ' + BW(1) + fg(0, 5, 0) + '>_' + reset + name
+    else
+        name
+
+extString = (ext) ->
+    key = if mediaTypes.has(ext) then "_media" else ext
+    colors[colors[key]? and key or '_default'][1] + '.' + ext + reset
+
+dirString = (name, ext) ->
+    c = name and '_dir' or '_.dir'
+    name = '📂 ' + name if args.icons
+    colors[c][0] + (name and (" " + name) or "") + (if ext then colors[c][1] + '.' + colors[c][2] + ext else "") + " "
 */
+
+func fileString(item *DisplayItem) string {
+	ext := strings.ToLower(item.ext)
+	var colors [2]string
+	if MediaTypes.Has(ext) {
+		colors = FileColor["_media"]
+	} else if CompressTypes.Has(ext) {
+		colors = FileColor["_compress"]
+	} else if val, hasExt := FileColor[ext]; hasExt {
+		colors = val
+	} else {
+		colors = FileColor["_default"]
+	}
+	ext = item.ext
+	if ext != "" {
+		ext = "." + ext
+	}
+	execIcon := ""
+	executable := item.info.Mode() & 0111
+	if executable != 0 && *args.icons {
+		execIcon = BgGray(1) + FgRGB(0, 5, 0) + ">_" + Reset + " "
+	}
+	displayStrings := []string{execIcon, colors[0], item.basename, colors[1], ext, Reset}
+	return strings.Join(displayStrings, "")
+}
+
+func dirString(item *DisplayItem) string {
+	colors := ConfigColor["dir"]
+	if item.basename == "" {
+		colors = ConfigColor[".dir"]
+	}
+	displayStrings := []string{colors["name"], " "}
+	if *args.icons {
+		displayStrings = append(displayStrings, "📂 ")
+	}
+	ext := item.ext
+	if ext != "" {
+		ext = "." + ext
+	}
+	displayStrings = append(displayStrings, item.basename, colors["ext"], ext, " ", Reset)
+	return strings.Join(displayStrings, "")
+}
+
+func permString(info os.FileInfo, ownerColor string, groupColor string) string {
+	defaultColor := PermsColor["other"]["_default"]
+	modeString := info.Mode().String()
+	coloredStrings := []string{defaultColor, modeString[0:1]}
+	coloredStrings = append(coloredStrings, ownerColor+modeString[1:4])
+	coloredStrings = append(coloredStrings, groupColor+modeString[4:7])
+	coloredStrings = append(coloredStrings, defaultColor+modeString[7:], Reset)
+	return strings.Join(coloredStrings, " ")
+}
+
+func sizeString(size int64) string {
+	for i, unit := range sizeUnits {
+		base := int64(math.Pow(10, float64(i*3)))
+		if size < base*1000 {
+			return SizeColor[unit] + pad.Left(fmt.Sprintf("%d", size/base), 4, " ") + unit + Reset
+		}
+	}
+	return strconv.Itoa(int(size))
+}
+
+func timeString(modtime time.Time) string {
+	dateStr := modtime.Format(dateFormat)
+	timeStr := modtime.Format(timeFormat)
+	hour, err := strconv.Atoi(timeStr[0:2])
+	check(err)
+	// generate a color based on the hour of the day. darkest around midnight and whitest around noon
+	timeColor := 14 - int(8*math.Cos(math.Pi*float64(hour)/12))
+	colored := []string{FgGray(22), dateStr, FgGray(timeColor) + timeStr, Reset}
+	return strings.Join(colored, " ")
+}
+
+// when we list out any subdirectories, print those paths conspicuously above the contents
+// this helps with visual separation
 func printFolderHeader(path string) {
-	headerString := ConfigColor["folderHeader"]["arrow"] + "►" + ConfigColor["header"]["main"] + " "
+	colors := ConfigColor["folderHeader"]
+	headerString := colors["arrow"] + "►" + colors["main"] + " "
+	prettyPath := prettifyPath(path)
+
+	if prettyPath == "/" {
+		headerString += "/"
+	} else {
+		folders := strings.Split(prettyPath, "/")
+		coloredFolders := make([]string, 0, len(folders))
+		for i, folder := range folders {
+			if i == len(folders)-1 { // different color for the last folder in the path
+				coloredFolders = append(coloredFolders, colors["lastFolder"]+folder)
+			} else {
+				coloredFolders = append(coloredFolders, colors["main"]+folder)
+			}
+		}
+		headerString += strings.Join(coloredFolders, colors["slash"]+"/")
+	}
+
+	fmt.Println(headerString + " " + Reset)
+}
+
+func prettifyPath(path string) string {
 	prettyPath, err := filepath.Abs(path)
 	check(err)
 	pwd := os.Getenv("PWD")
@@ -156,26 +366,45 @@ func printFolderHeader(path string) {
 	} else if strings.HasPrefix(prettyPath, home) {
 		prettyPath = "~" + prettyPath[len(home):]
 	}
+	return prettyPath
+}
 
-	if prettyPath == "/" {
-		headerString += "/"
-	} else {
-		folders := strings.Split(prettyPath, "/")
-		coloredFolders := make([]string, 0, len(folders))
-		for i, folder := range folders {
-			if i == len(folders)-1 {
-				coloredFolders = append(coloredFolders, ConfigColor["folderHeader"]["lastFolder"]+folder)
-			} else {
-				coloredFolders = append(coloredFolders, ConfigColor["folderHeader"]["main"]+folder)
-			}
-		}
-		headerString += strings.Join(coloredFolders, ConfigColor["folderHeader"]["slash"]+"/")
+func getOwnerAndGroup(fileInfo *os.FileInfo) (string, string) {
+	stat_t := (*fileInfo).Sys().(*syscall.Stat_t)
+	uid := fmt.Sprint(stat_t.Uid)
+	gid := fmt.Sprint(stat_t.Gid)
+	owner, err := user.LookupId(uid)
+	check(err)
+	group, err := user.LookupGroupId(gid)
+	check(err)
+	return owner.Username, group.Name
+}
+
+func getOwnerAndGroupColors(owner string, group string) (string, string) {
+	if owner == os.Getenv("USER") {
+		owner = "_self"
 	}
+	ownerColor := PermsColor["user"][owner]
+	if ownerColor == "" {
+		ownerColor = PermsColor["user"]["_default"]
+	}
+	groupColor := PermsColor["group"][group]
+	if groupColor == "" {
+		groupColor = PermsColor["group"]["_default"]
+	}
+	return ownerColor, groupColor
+}
 
-	fmt.Println(headerString + " " + Reset)
+// Go doesn't provide a `Max` function for ints like it does for floats (wtf?)
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func check(err error) {
+	// TODO: handle no permission error, broken links, invalid path args
 	if err != nil {
 		panic(err)
 	}
