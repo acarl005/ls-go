fe9fa625b6dfa574b9e5485c8a755384b7c8b6cb
Andy Carlson <andy@warp.dev>
2022-10-13 14:34:51 -0700

    update release script

--- release.sh
+++ release.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -ex
 
-if ! command -v github-release >/dev/null; then
-  echo must install github-release
-  echo go get github.com/github-release/github-release
+if ! command -v gh >/dev/null; then
+  echo must install gh
+  echo brew install gh
   exit 1
 fi
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo must set GITHUB_TOKEN >&2
-  exit 1
-fi
-
-TAG=0.2.1
+TAG=0.2.2
 
 git tag -a v$TAG -m "release v$TAG"
 
@@ -20,44 +15,5 @@ git push origin master --tags
 
 ./compile
 
-github-release release \
-  --user acarl005 \
-  --repo ls-go \
-  --tag v$TAG \
-  --name v$TAG
-
-
-github-release upload \
-  --user acarl005 \
-  --repo ls-go \
-  --tag v$TAG \
-  --name ls-go-darwin-amd64 \
-  --file ls-go-darwin-amd64
-
-github-release upload \
-  --user acarl005 \
-  --repo ls-go \
-  --tag v$TAG \
-  --name ls-go-darwin-arm64 \
-  --file ls-go-darwin-arm64
-
-github-release upload \
-  --user acarl005 \
-  --repo ls-go \
-  --tag v$TAG \
-  --name ls-go-linux-amd64 \
-  --file ls-go-linux-amd64
-
-github-release upload \
-  --user acarl005 \
-  --repo ls-go \
-  --tag v$TAG \
-  --name ls-go-linux-arm64 \
-  --file ls-go-linux-arm64
-
-github-release upload \
-  --user acarl005 \
-  --repo ls-go \
-  --tag v$TAG \
-  --name ls-go-linux-386 \
-  --file ls-go-linux-386
+gh release create v$TAG
+gh release upload v$TAG ls-go-*
